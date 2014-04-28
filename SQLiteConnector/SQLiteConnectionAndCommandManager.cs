cf88c052f777076fcfa4f17483cbced39e3780af
Paul Saunders <SpaceCuration@gmail.com>
2014-04-29 09:52:20 +1200

    Initial Commit

---
+++ SQLiteConnector/SQLiteConnectionAndCommandManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using CoreComponents.Data;
+
+namespace SQLiteConnector
+{
+
+    public class SQLiteConnectionAndCommandManager : ConnectionAndCommandManager<SQLiteConnection, SQLiteCommand, SQLiteParameter, SQLiteTransaction>
+    {
+
+        public SQLiteConnectionAndCommandManager()
+        {
+        }
+
+    }
+
+}
